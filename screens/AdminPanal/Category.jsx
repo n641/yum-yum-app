20450570830ba8622fc55ec165f47d3aa2c19800
yousseffninja <yousseffmohamedd22@gmail.com>
2022-04-26 04:49:25 +0200

    Styling of category

--- screens/AdminPanal/Category.jsx
+++ screens/AdminPanal/Category.jsx
@@ -4,21 +4,29 @@ import {
     TextInput,
     StyleSheet,
     Button,
-    FlatList
+    FlatList,
+    Dimensions
 } from "react-native";
 import { Ionicons } from "react-native-vector-icons";
 import { addCategory, getCategories } from "../../db/Auth/usersData/Categories";
 import CategoriesCard from '../Cards/CategoriesCart';
 
+    const width = Dimensions.get("window").width;
+    const height = Dimensions.get("window").height;
+
 const Category = ({ navigation }) => {
 
     const [category, setCategory] = useState("");
+    const [description, setDescription] = useState("");
+    const [imageLink, setImageLink] = useState("");
     const [arrCategory, setArrCategory] = useState([]);
 
     const addCategoryHandler = () => {
         console.log(category);
         addCategory({
             category: category,
+            description: description,
+            link: imageLink,
             products: [],
         })
     }
@@ -37,11 +45,27 @@ const Category = ({ navigation }) => {
                     placeholder="Enter category name"
                     style={styles.textInput}
                 />
-                <Ionicons 
+                <TextInput 
+                    onChangeText={setDescription}
+                    placeholder="Enter description"
+                    style={styles.textInputDescription}
+                />
+                <TextInput 
+                    onChangeText={setImageLink}
+                    placeholder="Enter image link"
+                    style={styles.textInput}
+                />
+                {/* <Ionicons 
                     name="send"
                     size={20}
                     onPress={() => addCategoryHandler()}
+                /> */}
+                <Button 
+                    title="add Category"
+                    onPress={() => addCategoryHandler()}
                 />
+            </View>
+            <View>
             </View>
             <View style={styles.getCategoriesContainer}>
                 <Button 
@@ -50,7 +74,13 @@ const Category = ({ navigation }) => {
                 />
                 <FlatList 
                     data={arrCategory}
-                    renderItem={itemData => <CategoriesCard category={itemData.item.category} />}
+                    numColumns={2}
+                    // style={{paddingVertical: 30}}
+                    renderItem={itemData => <CategoriesCard 
+                        category={itemData.item.category} 
+                        link={itemData.item.link}
+                        description={itemData.item.description}
+                    />}
                 />
             </View>
         </View>
@@ -60,12 +90,12 @@ const Category = ({ navigation }) => {
 const styles = StyleSheet.create({
     bigContainer: {
         flex: 1,
-        justifyContent: "center",
+        // justifyContent: "center",
         alignItems: "center",
+        width: width
     },
     Container: {
-        flexDirection: "row",
-        margin: 3
+        padding: 3
     },
     getCategoriesContainer: {
         margin: 3
@@ -74,6 +104,17 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         width: "100%",
         paddingVertical: 4,
+        marginVertical: 5,
+        justifyContent: "center",
+        alignContent: "center",
+        borderRadius: 10,
+        alignItems: "center"
+    },
+    textInputDescription: {
+        borderWidth: 1,
+        width: "100%",
+        paddingVertical: 25,
+        marginVertical: 5,
         justifyContent: "center",
         alignContent: "center",
         borderRadius: 10,
--- screens/Cards/CategoriesCart.jsx
+++ screens/Cards/CategoriesCart.jsx
@@ -2,13 +2,25 @@ import react from 'react';
 import { 
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image,
+    Dimensions
 } from 'react-native';
+
+    const width = Dimensions.get("window").width;
+    const height = Dimensions.get("window").height;
 
 const CategoriesCart = props => {
     return(
         <View style={styles.listItem}>
-            <Text>{props.category}</Text>
+            <Image 
+                source={{uri: props.link}}
+                style={{width: 100, height: 100}}
+            />
+            <View>
+                <Text style={styles.textHeader}>{props.category}</Text>
+                <Text style={styles.textDescription}>{props.description}</Text>
+            </View>
         </View>
     );
 }
@@ -18,12 +30,20 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         padding: 5,
         marginVertical: 5,
+        marginHorizontal: 5,
         backgroundColor: 'white',
         borderRadius: 10, 
         borderColor: 'black',
         borderWidth: 1,
-        overflow: "visible"
+        overflow: "visible",
+        width: width / 2.2,
     },
+    textHeader: {
+        fontWeight: 'bold'
+    },
+    textDescription: {
+        width: width / 4.3
+    }
 })
 
 export default CategoriesCart;
