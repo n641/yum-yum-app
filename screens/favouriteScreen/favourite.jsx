432175165da9bf22edc9015efe1b2e76ff167789
Noha Mohammed <74320801+n641@users.noreply.github.com>
2022-05-01 03:18:51 +0200

    handle favourite screen

--- screens/favouriteScreen/favourite.jsx
+++ screens/favouriteScreen/favourite.jsx
@@ -1,5 +1,9 @@
-import { StyleSheet, Text, View , Dimensions , FlatList,ScrollView , Image} from 'react-native'
-import React , {useState} from 'react';
+import { StyleSheet, Text, View, Dimensions, FlatList, ScrollView, Image } from 'react-native'
+import React, { useState, useEffect } from 'react';
+
+import { auth } from '../../db/config';
+import { getUsers, subscribeUser } from '../../db/Auth/usersData/users';
+import { getProducts, subscribe } from '../../db/Auth/usersData/Products';
 
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
@@ -7,41 +11,125 @@ const height = Dimensions.get("window").height;
 import FavCard from '../UserProfile/Components/Favourite/FavCard';
 import style from '../../Constants/style';
 
-const Favourite = ({ navigation}) => {
+const Favourite = ({ navigation }) => {
 
-    const [Favourite, setFavourite] = useState([]);
+  const [Favourite, setFavourite] = useState([]);
+  const [product, setproduct] = useState([]);
+
+
+  const [Users, setUsers] = useState([]);
+
+  const getUserss = async () => {
+    const arr = await getUsers();
+    setUsers(arr);
+    // console.log(arr);
+  };
+
+  const getProduct = async () => {
+    const arr = await getProducts();
+    setproduct(arr);
+    console.log(arr);
+  };
+
+  useEffect(() => {
+    getUserss();
+    getProduct();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = subscribeUser(({ change, snapshot }) => {
+      //   console.log("changes", change, snapshot, change.type);
+      // if (snapshot.metadata.hasPendingWrites) {
+      if (change.type === "added") {
+        console.log("New message: ", change.doc.data());
+        getUserss();
+      }
+      if (change.type === "modified") {
+        console.log("Modified city: ", change.doc.data());
+        getUserss();
+      }
+      if (change.type === "removed") {
+        console.log("Removed message: ", change.doc.data());
+        getUserss();
+      }
+      // }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = subscribe(({ change, snapshot }) => {
+      //   console.log("changes", change, snapshot, change.type);
+      // if (snapshot.metadata.hasPendingWrites) {
+      if (change.type === "added") {
+        console.log("New message: ", change.doc.data());
+        getProduct();
+      }
+      if (change.type === "modified") {
+        console.log("Modified city: ", change.doc.data());
+        getProduct();
+      }
+      if (change.type === "removed") {
+        console.log("Removed message: ", change.doc.data());
+        getProduct();
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+
+
+  useEffect(() => {
+    Users.map((e) =>
+      e.email == auth.currentUser.email
+        ? e.favourite.map((op) => (
+          setFavourite([...Favourite, op])
+        )
+        ) : null
+    )
+  })
 
   return Favourite.length != 0 ? (
     <ScrollView>
-        <Text
-          style={{
-            fontSize: 25,
-            color: style.primary,
-            fontWeight: "bold",
-            textAlign: "center",
-            marginVertical: 10,
-          }}
-        >
-          favourite meals
-        </Text>
+      <Text
+        style={{
+          fontSize: 25,
+          color: style.primary,
+          fontWeight: "bold",
+          textAlign: "center",
+          marginVertical: 10,
+        }}
+      >
+        favourite meals
+      </Text>
 
-        <FlatList
-          data={Favourite}
-          numColumns={2}
-          keyExtractor={(item) => item.name}
-          renderItem={(itemData) => (
-            <FavCard
-              name={itemData.item.name}
-              url={itemData.item.url}
-              price={itemData.item.price}
-              offer={itemData.item.offer}
-              discound={itemData.item.discound}
-              fav={itemData.item.fav}
-              desc={itemData.item.desc}
-              navigation={navigation}
-            />
-          )}
-        />
+      <FlatList
+        data={Favourite}
+        numColumns={2}
+        keyExtractor={(item) => item.name}
+        renderItem={(itemData) => (
+
+
+          <FavCard
+            name={itemData.item.name}
+            url={itemData.item.url}
+            price={itemData.item.price}
+            offer={itemData.item.offer}
+            discound={itemData.item.discound}
+            fav={itemData.item.fav}
+            desc={itemData.item.desc}
+            navigation={navigation}
+          />
+
+
+        )}
+      />
     </ScrollView>
   ) : (
     <View
@@ -79,3 +167,5 @@ const Favourite = ({ navigation}) => {
 }
 
 export default Favourite
+
+
