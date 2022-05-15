024d0de0cea020b0beb9d639414f47a62636778e
Noha Mohammed <74320801+n641@users.noreply.github.com>
2022-05-15 16:04:54 +0200

    add offers

--- screens/HomeScreen/Components/Offer.jsx
+++ screens/HomeScreen/Components/Offer.jsx
@@ -7,8 +7,9 @@ import {
   Image,
   ScrollView,
 } from "react-native";
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import { auth } from "../../../db/config";
+import { getOffers , subscribe } from "../../../db/Auth/usersData/Offers";
 
 import OfferCard from "./OfferCard";
 
@@ -17,37 +18,70 @@ const height = Dimensions.get("window").height;
 
 const Offers = ({ navigation}) => {
  const [pro, setpro] = useState([
-      //must order product by count!!!!
-      {
-        name: "sawarmaa",
-        url: "https://pbs.twimg.com/media/EoyE2lvWEAAo-pk?format=jpg&name=4096x4096",
-        price: 20,
-        count: 19,
-        offer: true,
-        discound: 20,
-        fav: true,
-        desc: "pla plap pla pla pla pla pla pla pla pla pl apl apl apl pal pal pal pal pa lwerrrrrrrrrrrrrrrrrrr",
-      },
-      {
-        name: "pizza",
-        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn_hPABuSXp3vmpfoOhZASRFB3O1qfF8c_Ew&usqp=CAU",
-        price: 70,
-        count: 8,
-        offer: true,
-        discound: 10,
-        fav: true,
-        desc: "pla plap pla pla pla pla pla pla pla pla pl apl apl apl pal pal pal pal pa lwerrrrrrrrrrrrrrrrrrr",
-      },
-      {
-        name: "burger",
-        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_w3pS-DmxibqgtTz2H2FLuCIs5dmUl9YB5g&usqp=CAU",
-        price: 100,
-        count: 7,
-        offer: false,
-        discound: 10,
-        fav: false,
-        desc: "pla plap pla pla pla pla pla pla pla pla pl apl apl apl pal pal pal pal pa lwerrrrrrrrrrrrrrrrrrr",
-      },])
+      // {
+      //   name: "sawarmaa",
+      //   url: "https://pbs.twimg.com/media/EoyE2lvWEAAo-pk?format=jpg&name=4096x4096",
+      //   price: 20,
+      //   count: 19,
+      //   offer: true,
+      //   discound: 20,
+      //   fav: true,
+      //   desc: "pla plap pla pla pla pla pla pla pla pla pl apl apl apl pal pal pal pal pa lwerrrrrrrrrrrrrrrrrrr",
+      // },
+      // {
+      //   name: "pizza",
+      //   url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn_hPABuSXp3vmpfoOhZASRFB3O1qfF8c_Ew&usqp=CAU",
+      //   price: 70,
+      //   count: 8,
+      //   offer: true,
+      //   discound: 10,
+      //   fav: true,
+      //   desc: "pla plap pla pla pla pla pla pla pla pla pl apl apl apl pal pal pal pal pa lwerrrrrrrrrrrrrrrrrrr",
+      // },
+      // {
+      //   name: "burger",
+      //   url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_w3pS-DmxibqgtTz2H2FLuCIs5dmUl9YB5g&usqp=CAU",
+      //   price: 100,
+      //   count: 7,
+      //   offer: false,
+      //   discound: 10,
+      //   fav: false,
+      //   desc: "pla plap pla pla pla pla pla pla pla pla pl apl apl apl pal pal pal pal pa lwerrrrrrrrrrrrrrrrrrr",
+      // },
+    
+    ])
+
+      const getItems = async () => {
+        const arr = await getOffers();
+        setpro(arr);
+      };
+
+      useEffect(() => {
+        getItems();
+      }, []);
+
+
+      useEffect(() => {
+        const unsubscribe = subscribe(({ change, snapshot }) => {
+          if (change.type === "added") {
+            getItems();
+          }
+          if (change.type === "modified") {
+            getItems();
+          }
+          if (change.type === "removed") {
+            getItems();
+          }
+          // }
+        });
+      
+        return () => {
+          unsubscribe();
+        };
+      }, []);
+      
+
+      
 
   return (
     <View
@@ -65,6 +99,7 @@ const Offers = ({ navigation}) => {
         bounces={false}
       >
         {pro.map((e, id) => (
+          // e.offer==true?(
           <OfferCard
             url={e.url}
             desc={e.desc}
@@ -76,6 +111,7 @@ const Offers = ({ navigation}) => {
             name={e.name}
             navigation={navigation}
           />
+          // ):null
         ))}
       </Animated.ScrollView>
     </View>
